0cc961155292f3fa78c4d530dd1be5818abab253
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-01-25 22:44:55 +0500

    Next JS create app

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+export const metadata = {
+  title: 'Zoyidjon App | About',
+  description: 'React App with TypeScript and Webpack',
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>About Page</h1>
+    </>
+  )
+}
+
+export default AboutPage
---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'Zoyidjon App',
+  description: 'React App with TypeScript and Webpack',
+};
+
+
+const IndexPage = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is a simple React app with TypeScript and Webpack.</p>
+    </>
+  )
+}
+
+export default IndexPage

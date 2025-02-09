c427ea79d373a6116f4aaa74502411cfc8f86d45
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-09 19:10:10 +0500

    SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React from 'react';
 import Layout from '../layout';
 import { Hero, Sidebar, Content } from '../components';
 import { Box } from '@mui/material';
-
+import { BlogsServices } from '../services/blog.services';
+import { BlogsType } from '../interface/blogs.interface';
 
 export const metadata = {
   title: 'Zoyidjon Blog App',
   description: 'React App with TypeScript and Webpack',
 };
 
+const getBlogs = async (): Promise<BlogsType[]> => {
+  const blogs = await BlogsServices.getAllBlogs();
+  return blogs;
+};
 
-const IndexPage = () => {
-  return (  
+const IndexPage = async () => {
+  const blogs = await getBlogs(); // Ma'lumotlarni server tomonida olish
+  console.log(blogs);
+  
+
+  return (
     <Layout>
       <Hero />
-      <Box sx={{ display: 'flex', gap: "20px" , flexDirection: {xs : 'column', md: 'row' },  padding: '20px'}}>
+      <Box sx={{ display: 'flex', gap: "20px", flexDirection: { xs: 'column', md: 'row' }, padding: '20px' }}>
         <Sidebar />
-        <Content />
+        <Content />  {/* blogs ni Content komponentiga berish */}
       </Box>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
--- src/services/blog.services.ts
+++ src/services/blog.services.ts
@@ -1,6 +1,8 @@
 import { request, gql } from 'graphql-request'
+import { BlogsType } from '../interface/blogs.interface';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT as string;
+const token = process.env.NEXT_PUBLIC_HYGRAPH_TOKEN as string;
 
 export const BlogsServices = {
   async getAllBlogs() {
@@ -29,11 +31,13 @@ export const BlogsServices = {
     `
 
     try {
-      const data = await request(graphqlAPI, query) // Fixed the variable name here
-      return data.blogs
+      const data = await request<{ blogs: BlogsType[] }>(graphqlAPI, query, {}, {
+        Authorization: `Bearer ${token}`,  // 👈 API token qo'shildi
+      });
+      return data.blogs;
     } catch (error) {
-      console.error('Error fetching blogs:', error)
-      return []
+      console.error("GraphQL Request Error:", error);
+      return [];
     }
   }
 }
